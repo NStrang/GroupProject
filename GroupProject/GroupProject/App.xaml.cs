b659ec72292a0fd01a738a16d5c824639f3b93e3
0773019 <0773019@KELCE-ACIS07-P>
2019-11-04 17:26:30 -0600

    Added Navigation to app.xaml.cs

--- GroupProject/GroupProject/App.xaml.cs
+++ GroupProject/GroupProject/App.xaml.cs
@@ -11,6 +11,7 @@ namespace GroupProject
             InitializeComponent();
 
             MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
